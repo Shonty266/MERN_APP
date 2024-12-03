edf98acdab23834572089ba3b5809c3dd141b03e
Shonty266 <aryanhinglajia2663@gmail.com>
2024-12-03 17:18:11 +0530

    Your commit message here

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Toast from './toastNotification/Toast'
 import loginImg from './assets/login-img.svg'
